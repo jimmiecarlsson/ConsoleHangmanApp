3a5aee0b1e29e22d418399e54a392224ef98070a
Jimmie Carlsson <jimpa.c66@gmail.com>
2025-08-11 11:19:34 +0200

    Add begining for the app

--- ConsoleHangmanApp/Program.cs
+++ ConsoleHangmanApp/Program.cs
@@ -1,11 +1,33 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("\nHangman Game");
 
 
 bool appIsRunning = true;
 
 while (appIsRunning)
 {
+Console.WriteLine("\nHangman Game");
+    Console.WriteLine("S. Start Game");
+    Console.WriteLine("Q. Exit");   
+    
+    Console.Write("Choose an option: ");
+    string? input = Console.ReadLine();
+
+    switch{
+
+        case "s":
+            Console.WriteLine("Starting the game...");
+            // Here you would call the method to start the game
+            // For example: StartGame();
+
+            break;
+        case "q":
+            Console.WriteLine("Exiting the game. Goodbye!");
+            appIsRunning = false;
+            break;
+        default:
+            Console.WriteLine("Invalid option, please try again.");
+            break;
+    };
 
 
 
