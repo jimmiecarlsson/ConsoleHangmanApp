2393c31a5dc52685fbec1ed847d919282f248212
Jimmie Carlsson <jimpa.c66@gmail.com>
2025-08-12 17:01:50 +0200

    Add logic to the game mechanics

--- ConsoleHangmanApp/Actions/StartGame.cs
+++ ConsoleHangmanApp/Actions/StartGame.cs
@@ -1,11 +1,10 @@
 ﻿using System;
-using System.Diagnostics.Metrics;
 
 namespace ConsoleHangmanApp.Actions
 {
     public class StartGame
     {
-        string chooseWord = Words.GetRandomWord();
+        string chooseWord = Words.GetRandomWord().ToLower();
         char letter = ' ';
         List<char> guessedLetters = new List<char>();
         int maxAttempts = 10;
@@ -28,12 +27,15 @@ namespace ConsoleHangmanApp.Actions
             Console.WriteLine("Guess a letter or \npress SPACE to guess the whole word:");
 
             letter = Console.ReadKey().KeyChar;
+
             // Check if the input is a space character
             if (letter == ' ')
             {
                 Console.WriteLine("\nYou chose to guess the whole word.");
                 Console.Write("Please enter your guess: ");
-                string wordGuess = Console.ReadLine()?.Trim().ToLower();
+            
+                string wordGuess = Console.ReadLine().ToLower();
+                
                 if (wordGuess == chooseWord)
                 {
                     CheckWinCondition();
@@ -41,36 +43,59 @@ namespace ConsoleHangmanApp.Actions
                 else
                 {
                     Console.WriteLine($"Sorry, '{wordGuess.ToUpper()}' is not the correct word.");
-                
                 }
                 return;
             }
+
+            // Check if the input is a valid letter (A-Ö)
             if (!char.IsLetter(letter) && letter != ' ')
             {
                 Console.WriteLine("\nPlease enter a letter (A-Ö).");
                 return;
             }
 
+
             // add the guessed letter to the list
-            guessedLetters.Add(letter);
+            guessedLetters.Add(char.ToLower(letter));
 
-            string letterString = letter.ToString().ToLower();
-
+            // Print the guessed letter
             Console.WriteLine();
             Console.WriteLine($"You guessed the letter: {letter.ToString().ToUpper()}");
 
+            // Check if the letter is in the word
+            string letterString = letter.ToString().ToLower();
             bool isLetterInWord = chooseWord.Contains(letterString);
 
             if (isLetterInWord)
             {
-                Console.WriteLine($"Correct! The letter '{letter.ToString().ToUpper()}' is in the word.");
-                correctGuesses[correctGuessCount] = letter.ToString().ToUpper();
+                for (int i = 0; i < correctGuessCount; i++)
+                {
+                    if (correctGuesses[i] != null && correctGuesses[i] == char.ToString(letter).ToLower())
+                    {
+                        Console.WriteLine($"You have already guessed the letter '{letter.ToString().ToUpper()}'.");
+                        return;
+                    }
+
+                }
+
+                Console.WriteLine($"Correct! The letter '{letter.ToString().ToUpper()}' is in the word.\n");
+                correctGuesses[correctGuessCount] = letter.ToString().ToLower();
                 correctGuessCount++;
             }
             else
             {
-                Console.WriteLine($"Incorrect! The letter '{letter.ToString().ToUpper()}' is not in the word.");
-                incorrectGuesses[incorrectGuessCount] = letter.ToString().ToUpper();
+                for (int i = 0; i < incorrectGuessCount; i++)
+                {
+                    if (incorrectGuesses[i] != null && incorrectGuesses[i] == char.ToString(letter).ToLower())
+                    {
+                        Console.WriteLine($"You have already guessed the letter '{letter.ToString().ToUpper()}'.");
+                        return;
+                    }
+
+                }
+
+                Console.WriteLine($"Incorrect! The letter '{letter.ToString().ToUpper()}' is not in the word.\n");
+                incorrectGuesses[incorrectGuessCount] = letter.ToString().ToLower();
                 incorrectGuessCount++;
             }
 
@@ -79,10 +104,13 @@ namespace ConsoleHangmanApp.Actions
 
         public void CheckWinCondition()
         {
+
             Console.WriteLine($"Congratulations! You guessed the word '{chooseWord.ToUpper()}' correctly!");
         }
         public void DisplayCurrentState()
         {
+            Console.WriteLine("\nCorrect choosen letters.");
+
             foreach (char c in chooseWord)
             {
                 if (guessedLetters.Contains(char.ToLower(c)))
@@ -100,7 +128,7 @@ namespace ConsoleHangmanApp.Actions
                     Console.ResetColor();
                 }
             }
-            Console.WriteLine("Incorrect choosen letter.");
+            Console.WriteLine("\nIncorrect choosen letter so far.");
             for (int i = 0; i < incorrectGuessCount+1; i++)
             {
                 if (incorrectGuesses[i] != "")
